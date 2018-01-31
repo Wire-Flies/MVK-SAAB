ba22c2471c79eb9d3819c1896d61a9fcb3656a42
Anton Ivarsson <antonivarsson@Antons-MacBook-Pro.local>
2018-01-31 20:28:00 +0100

    Added firebase init

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,3 +1,30 @@
+'use strict';
+const express = require("express");
+const app = express();
+const {FirebaseConnector} = require('./firebase.js');
 
-console.log('Running app...');
+let firebaseConnector = new FirebaseConnector();
 
+init().then(() => {
+    console.log('Ready...');
+}).catch((err) => {
+    console.log('There was an initializing');
+    console.log(err);
+});
+
+function initFirebase(){
+    return firebaseConnector.login();
+}
+
+function startApp(){
+    return new Promise((resolve, reject) => {
+        app.listen(3000, '127.0.0.1', () => resolve());
+    });
+}
+
+function init(){
+    return Promise.all([
+        initFirebase(),
+        startApp()
+    ]);
+}
