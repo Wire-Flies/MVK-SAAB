7359893c6aac122f196dada19e9905909111f786
Gabriel Gessle <sandshrew97@gmail.com>
2018-02-07 23:34:50 +0100

    Removed more files we don't need also fixed minor bug. Now prints all anomalies for the user in the console

--- cesium_frontend/app.js
+++ cesium_frontend/app.js
@@ -1,15 +1,24 @@
+// firebase stuff
 const email = "Saab2@blufffmail.com";
 const password = "Saab2123";
 var userId, anomalies;
 
 // initiate firebase connection
 initFireBase();
-signInFirebase();
+signInFirebase(email, password);
 
 // cesium stuff
+/* example how to spawn an entity (from their tutorial)
 var viewer = new Cesium.Viewer('cesiumContainer');
+var entity = viewer.entities.add({
+    position : Cesium.Cartesian3.fromDegrees(-123.0744619, 44.0503706),
+    model : {
+        uri : '/Apps/SampleData/models/CesiumGround/Cesium_Ground.gltf'
+    }
+});
+*/
 
 function spawnAnomalies() {
-    // create anoamlies with cesium
+    // spawn anomalies
 }
 
