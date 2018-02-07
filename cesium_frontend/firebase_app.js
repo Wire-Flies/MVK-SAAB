8a1bd7ab092dfcdb52856903c146c2b2e16b813d
Gabriel Gessle <sandshrew97@gmail.com>
2018-02-07 23:22:50 +0100

    Basic firebase integration, can log in and retrieve anomaly data. Still using the Cesium server included in their package

---
+++ cesium_frontend/firebase_app.js
@@ -0,0 +1,47 @@
+const email = "Saab2@blufffmail.com";
+const password = "Saab2123";
+var userId, anomalies;
+
+/**
+ * Initiates our firebase connection
+ */
+function initFireBase() {
+    firebase.initializeApp({
+     apiKey: "AIzaSyAWdgHMu_1yOOWZEyXJWRV_BibqNWfZQZA",
+     authDomain: "mvk-saab.firebaseapp.com",
+     databaseURL: "https://mvk-saab.firebaseio.com",
+     projectId: "mvk-saab",
+     storageBucket: "mvk-saab.appspot.com",
+     messagingSenderId: "451451428506"
+   })
+}
+
+/**
+ * Signs the user into firebase
+ * @param {String} email - Email to log in with
+ * @param {String} password - Password to try
+ */
+function signInFirebase(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch((error) => {
+        console.log("An error has occured during user authentication\n Error code: " + error.code + "\n Error Message: " + error.message);
+    })
+    .then(() => {
+        console.log("User " + email + " has been authenticated");
+        userId = firebase.auth().currentUser.uid;
+
+        //Right when we succeed with authentication, get all anomalies
+        getDataFirebase(userId);
+    })
+}
+
+/**
+ * Retrieves all anomalies from the database
+ * @param {String} userId - User ID used in the firebase db 
+ */
+function getDataFirebase(userId) {
+    firebase.database().ref('/users/' + userId).once('value')
+    .then((snapshot) => {
+        anomalies = snapshot.val().anomalies;
+        console.log(anomalies);
+    });
+}
