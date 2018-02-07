3fc765b24f9d6dc295e1cb20f50df5b4d442992c
Gabriel Gessle <sandshrew97@gmail.com>
2018-02-07 23:22:50 +0100

    Basic firebase integration, can log in and retrieve anomaly data. Still using the Cesium server included in their package

---
+++ cesium_frontend/app.js
@@ -0,0 +1,15 @@
+const email = "Saab2@blufffmail.com";
+const password = "Saab2123";
+var userId, anomalies;
+
+// initiate firebase connection
+initFireBase();
+signInFirebase();
+
+// cesium stuff
+var viewer = new Cesium.Viewer('cesiumContainer');
+
+function spawnAnomalies() {
+    // create anoamlies with cesium
+}
+
