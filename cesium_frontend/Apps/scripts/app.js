ad6bc5207a6f269bc55707ac7acae9068a8322be
Gabriel Gessle <sandshrew97@gmail.com>
2018-02-09 01:06:21 +0100

    Anomalies are now a different color when new (click to change), anomalies have a description, data boundary shown, disabled camera look, removed some default ui buttons

--- cesium_frontend/Apps/scripts/app.js
+++ cesium_frontend/Apps/scripts/app.js
@@ -10,24 +10,60 @@ signInFirebase(email, password);
 // -- CESIUM --
 // properties
 var anomalySize = 5000;
-var res = 2;
-var maxZoom = 5000000.0;
+var res = 3;
+var maxZoom = 8000000.0;
 var anomalies, dbRef;
+var newAnomalyColor = Cesium.Color.GREEN;
+var oldAnomalyColor = Cesium.Color.GOLD;
 
 // set up viewer
 var viewer = new Cesium.Viewer('cesiumContainer', {
     sceneMode: Cesium.SceneMode.SCENE2D,
     sceneModePicker: false,
-    mapProjection : new Cesium.WebMercatorProjection()
+    mapProjection : new Cesium.WebMercatorProjection(),
+    homeButton: false,
+    sceneModePicker: false,
+    navigationHelpButton: false,
 });
 viewer.resolutionScale = res;
 var scene = viewer.scene;
+
+// remove double click feature
+viewer.cesiumWidget.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
 
 // center camera and limit zoom
 // - todo - maybe look at a new way to limit what the camera sees
 var center = Cesium.Cartesian3.fromDegrees(18.0649, 59.33258);
 viewer.camera.lookAt(center, new Cesium.Cartesian3(0.0, 0.0, maxZoom));
 scene.screenSpaceCameraController.maximumZoomDistance = maxZoom;
+scene.screenSpaceCameraController.enableLook = false;
+
+// create ellipse to show where data will be showed
+var boundaryEllipse = viewer.entities.add({
+    id: "data-boundary",
+    position: center,
+    name: "Data boundary",
+    ellipse: {
+        semiMinorAxis: 1000000,
+        semiMajorAxis: 1000000,
+        material: Cesium.Color.RED.withAlpha(0.15)
+    }
+});
+
+// define the entity color change on-left-click event listener
+colorHandler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
+colorHandler.setInputAction((movement) => {
+    var clickedObject = scene.pick(movement.position);
+    if(Cesium.defined(clickedObject) && (clickedObject.id._id !== "data-boundary")) {
+        var entity = viewer.entities.getById(clickedObject.id._id);
+        entity['model']['color'] = oldAnomalyColor;
+    }
+}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+// -EXPERIMENTAL!- define our clock tick event listener
+/*viewer.clock.onTick.addEventListener((clock) => {
+    console.log(clock.currentTime);
+});*/
 
 /**
  * Spawns every anomaly in the 'anomalies' object based on their longitude,
@@ -37,6 +73,7 @@ scene.screenSpaceCameraController.maximumZoomDistance = maxZoom;
 function spawnAnomalies(anomalies) {
     // spawn anomalies
     for(var anomalyId in anomalies) {
+        console.log(anomalies);
         console.log("SPAWNING ENTITY:");
         var anomaly = anomalies[anomalyId];
         console.log(anomaly);
@@ -49,17 +86,22 @@ function spawnAnomalies(anomalies) {
         var heading = new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(anomaly.heading), 0, 0);
         var orientation = Cesium.Transforms.headingPitchRollQuaternion(position, heading)
 
+        // fix description text
+        var description = "(latitude, longitude): (" + anomaly.latitude + ", " + anomaly.longitude + ")<br>Altitude: " + anomaly.altitude + 
+            " feet<br>Heading: " + anomaly.heading + "°<br>Speed: " + anomaly.speed + " knots<br>Squawk: " + anomaly.squawk;
+
         /* Check if anomaly already exists, if so modify the old one.
          Otherwise create a new one. */
         if(!viewer.entities.getById(anomalyId)) {
             var entity = viewer.entities.add({
                 id: anomalyId,
+                name: anomaly.flight_id,
                 position: position,
                 orientation: orientation,
-                description: 'blablobla xD',
+                description: description,
                 model : {
                     uri : '/Apps/SampleData/models/CesiumAir/Cesium_Air.gltf',
-                    color: Cesium.Color.GOLD,
+                    color: newAnomalyColor,
                     scale: anomalySize
                 }
             });
@@ -67,6 +109,9 @@ function spawnAnomalies(anomalies) {
             var entity = viewer.entities.getById(anomalyId);
             entity['position'] = position;
             entity['orientation'] = orientation;
+            /* -TODO- CHANGE ANOMALY COLOR WHEN REPOSITIONED?
+            entity['model']['color'] = newAnomalyColor;
+            */
         }
 
         console.log("SPAWNED ENTITY"); 
