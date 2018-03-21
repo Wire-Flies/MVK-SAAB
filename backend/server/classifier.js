c8353be5feb6a504d1479a2cb9c6d0ab10fc8450
jvikstrom <johan.vikstrom7@gmail.com>
2018-03-21 23:53:13 +0100

    Added rpc calls from backend

---
+++ backend/server/classifier.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const fetch = require('node-fetch');
+const URL = 'http://' + process.env.AI_HOST + ':' + process.env.AI_PORT;
+
+module.exports = {
+    classify: classify
+};
+
+/**
+ * Classifies a plane
+ * 
+ * @param {Object} plane    Plane object that should be classified
+ * @returns {Promise}       Promise that resolves to the grade that an airplane is an anomaly 
+ */
+function classify(plane){
+    const headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    };
+
+    return fetch(URL + '/classify', {
+        method: 'POST',
+        json: true,
+        body: JSON.stringify(plane),
+        headers: headers
+    }).then(res => res.json()).then((body) => {
+        return new Promise((resolve) => resolve(body.value));        
+    });
+}
