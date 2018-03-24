d337f210858799e7937a2ab90b785d818f9a1849
Anton Ivarsson <anton.ivarsson@allabrf.se>
2018-03-24 11:45:53 +0100

    Firebaselogin returns a promise with uid

--- backend/server/firebase.js
+++ backend/server/firebase.js
@@ -2,65 +2,93 @@
 const firebase = require('firebase');
 
 /*
-Firebase init credentials
+    Firebase init credentials
 */
-let usr = '';
-//Firebase user credentials
-const  email = 'Saab2@blufffmail.com';
-const  password = 'Saab2123';
+const email = 'Saab2@blufffmail.com';
+const password = 'Saab2123';
 
-class FirebaseConnector{
-    constructor(){
+/**
+ * FirebaseConnector class, used for communicating with firebase
+*/
+class FirebaseConnector {
+    /**
+     * Constructor, initializes connection with firebase
+     */
+    constructor() {
         initFirebase();
     }
 
-    login(){
+    /**
+     * Logs in
+     * @return {Promise} Resolved when logged in
+     */
+    login() {
         return firebaseLogin().then((usr) => {
             this.usr = usr;
             return new Promise((full) => full());
         });
     }
 
-    saveData(flightId, snapshots){
+    /**
+     * Save data to flightradar
+     * @param {String} flightId     Id of flight
+     * @param {Array} snapshots         Data of flight positions
+     * @return {Promise}    Resolved when done saving
+     */
+    saveData(flightId, snapshots) {
         return saveData(this.usr, flightId, snapshots);
     }
 }
 
-function initFirebase(){
+/**
+ * Initializes firebase
+ */
+function initFirebase() {
     firebase.initializeApp({
         apiKey: 'AIzaSyAWdgHMu_1yOOWZEyXJWRV_BibqNWfZQZA',
         authDomain: 'mvk-saab.firebaseapp.com',
         databaseURL: 'https://mvk-saab.firebaseio.com',
         projectId: 'mvk-saab',
         storageBucket: 'mvk-saab.appspot.com',
-        messagingSenderId: '451451428506'
+        messagingSenderId: '451451428506',
     });
 }
 
-//TODO: This function must return a promise with the "user id"
-function firebaseLogin(){
+/**
+ * Logs in to firebase
+ * @return {Promise}    Resolves when done logging in
+ */
+function firebaseLogin() {
     return new Promise((resolve, reject) => {
         firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
-            const userId = '';
+            const userId = firebase.auth().getUid();
             resolve(userId);
         }).catch((loginErr) => {
             firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
-                const userId = '';
+                const userId = firebase.auth().getUid();
                 resolve(userId);
             }).catch((err) => {
                 reject(err);
-            })
+            });
         });
     });
 }
 
-function saveData(usr, flightId, snapshots){
+/**
+ * Save data
+ *
+ * @param {String} usr  User it should be saved at
+ * @param {String} flightId  Id of flight
+ * @param {Array} snapshots Data of flight positions
+ * @return {Promise}    Resolved when done saving
+ */
+function saveData(usr, flightId, snapshots) {
     return firebase.database().ref('users/' + usr + '/anomalies').push({
         flight_ID: flightId,
-        snapShots: snapshots
+        snapShots: snapshots,
     });
 }
 
 module.exports = {
-    FirebaseConnector: FirebaseConnector
+    FirebaseConnector: FirebaseConnector,
 };
--- backend/server/test/firebase.test.js
+++ backend/server/test/firebase.test.js
@@ -7,134 +7,137 @@ let firebase = rewire('../firebase.js');
 
 let authMock = {
     signInWithEmailAndPassword: sinon.stub(),
-    createUserWithEmailAndPassword: sinon.stub()
+    createUserWithEmailAndPassword: sinon.stub(),
+    getUid: sinon.stub(),
 };
 let refMock = {
-    push: sinon.stub()
+    push: sinon.stub(),
 };
 let databaseMock = {
-    ref: () => refMock
+    ref: () => refMock,
 };
 let firebaseMock = {
     initializeApp: sinon.stub(),
     database: () => databaseMock,
-    auth: () => authMock
+    auth: () => authMock,
 };
 
 let firebaseConnector;
-describe('firebase.js', function(){
-    beforeEach(function(){
+describe('firebase.js', function() {
+    beforeEach(function() {
         authMock = {
             signInWithEmailAndPassword: sinon.stub().rejects(),
-            createUserWithEmailAndPassword: sinon.stub().rejects()
+            createUserWithEmailAndPassword: sinon.stub().rejects(),
         };
         refMock = {
-            push: sinon.stub().rejects()
+            push: sinon.stub().rejects(),
         };
         databaseMock = {
-            ref: () => refMock
+            ref: () => refMock,
         };
         firebaseMock = {
             initializeApp: sinon.stub(),
             database: () => databaseMock,
-            auth: () => authMock
+            auth: () => authMock,
         };
         firebaseConnector = new firebase.FirebaseConnector();
         firebase.__set__('firebase', firebaseMock);
     });
-    describe('login()', function(){
-        describe('successfull login', function(){
-            beforeEach(function(){
+    describe('login()', function() {
+        describe('successfull login', function() {
+            beforeEach(function() {
                 authMock = {
                     signInWithEmailAndPassword: sinon.stub().resolves(),
-                    createUserWithEmailAndPassword: sinon.stub().rejects()
+                    createUserWithEmailAndPassword: sinon.stub().rejects(),
+                    getUid: sinon.stub().returns('Akkjzumle0WDIxaUrvSVtawj98z1'),
                 };
                 return firebaseConnector.login();
             });
-            it('calls signInWithEmailAndPassword', function(){
+            it('calls signInWithEmailAndPassword', function() {
                 sinon.assert.calledOnce(authMock.signInWithEmailAndPassword);
                 sinon.assert.calledWithMatch(authMock.signInWithEmailAndPassword, sinon.match.string, sinon.match.string);
             });
-            it('does not call createUserWithEmailAndPassword', function(){
+            it('does not call createUserWithEmailAndPassword', function() {
                 sinon.assert.notCalled(authMock.createUserWithEmailAndPassword);
             });
-            it('has set user to <TODO>', function(){
-                assert.equal(firebaseConnector.usr, '<TODO>');
+            it('has set user to \"firebase.auth.getUid()\"', function() {
+                assert.equal(firebaseConnector.usr, 'Akkjzumle0WDIxaUrvSVtawj98z1');
             });
         });
-        describe('successfull create user', function(){
-            beforeEach(function(){
+        describe('successfull create user', function() {
+            beforeEach(function() {
                 authMock = {
                     signInWithEmailAndPassword: sinon.stub().rejects(),
-                    createUserWithEmailAndPassword: sinon.stub().resolves()
+                    createUserWithEmailAndPassword: sinon.stub().resolves(),
+                    getUid: sinon.stub().returns('Akkjzumle0WDIxaUrvSVtawj98z1'),
                 };
                 return firebaseConnector.login();
             });
-            it('calls signInWithEmailAndPassword', function(){
+            it('calls signInWithEmailAndPassword', function() {
                 sinon.assert.calledOnce(authMock.signInWithEmailAndPassword);
                 sinon.assert.calledWithMatch(authMock.signInWithEmailAndPassword, sinon.match.string, sinon.match.string);
             });
-            it('calls createUserWithEmailAndPassword', function(){
+            it('calls createUserWithEmailAndPassword', function() {
                 sinon.assert.calledOnce(authMock.createUserWithEmailAndPassword);
                 sinon.assert.calledWithMatch(authMock.createUserWithEmailAndPassword, sinon.match.string, sinon.match.string);
             });
-            it('has set user to <TODO>', function(){
-                assert.equal(firebaseConnector.usr, '<TODO>');
+            it('has set user to \"firebase.auth.getUid()\"', function() {
+                assert.equal(firebaseConnector.usr, 'Akkjzumle0WDIxaUrvSVtawj98z1');
             });
         });
-        describe('unsuccessfull login', function(){
-            beforeEach(function(done){
+        describe('unsuccessfull login', function() {
+            beforeEach(function(done) {
                 authMock = {
                     signInWithEmailAndPassword: sinon.stub().rejects(),
-                    createUserWithEmailAndPassword: sinon.stub().rejects()
+                    createUserWithEmailAndPassword: sinon.stub().rejects(),
                 };
                 firebaseConnector.login().then(() => done(new Error())).catch(() => done());
             });
-            it('calls signInWithEmailAndPassword', function(){
+            it('calls signInWithEmailAndPassword', function() {
                 sinon.assert.calledOnce(authMock.signInWithEmailAndPassword);
                 sinon.assert.calledWithMatch(authMock.signInWithEmailAndPassword, sinon.match.string, sinon.match.string);
             });
-            it('calls createUserWithEmailAndPassword', function(){
+            it('calls createUserWithEmailAndPassword', function() {
                 sinon.assert.calledOnce(authMock.createUserWithEmailAndPassword);
                 sinon.assert.calledWithMatch(authMock.createUserWithEmailAndPassword, sinon.match.string, sinon.match.string);
             });
-            it('has not set user to <TODO>', function(){
-                assert.notEqual(firebaseConnector.usr, '<TODO>');
+            it('has not set user to \"firebase.auth.getUid()\"', function() {
+                assert.notEqual(firebaseConnector.usr, '');
             });
         });
     });
-    describe('saveData()', function(){
-        describe('successfull save data', function(){
-            beforeEach(function(){
+    describe('saveData()', function() {
+        describe('successfull save data', function() {
+            beforeEach(function() {
                 refMock = {
-                    push: sinon.stub().resolves()
+                    push: sinon.stub().resolves(),
                 };
 
                 firebaseConnector.usr = 'fbuser';
                 return firebaseConnector.saveData('flyid', [{},{}]);
             });
-            it('calls push', function(){
+            it('calls push', function() {
                 sinon.assert.calledOnce(refMock.push);
                 sinon.assert.calledWithMatch(refMock.push, {
                     flight_ID: 'flyid',
-                    snapShots: [{},{}]
+                    snapShots: [{}, {}],
                 });
             });
         });
-        describe('unsuccessful save data', function(){
-            beforeEach(function(done){
+        describe('unsuccessful save data', function() {
+            beforeEach(function(done) {
                 refMock = {
-                    push: sinon.stub().rejects()
+                    push: sinon.stub().rejects(),
                 };
 
                 firebaseConnector.usr = 'fbuser';
-                firebaseConnector.saveData('flyid', [{},{}]).then(() => done(new Error())).catch(() => done());
+                firebaseConnector.saveData('flyid', [{}, {}]).then(() => done(new Error())).catch(() => done());
             });
-            it('calls push', function(){
+            it('calls push', function() {
                 sinon.assert.calledOnce(refMock.push);
                 sinon.assert.calledWithMatch(refMock.push, {
                     flight_ID: 'flyid',
-                    snapShots: [{},{}]
+                    snapShots: [{}, {}],
                 });
             });
         });
