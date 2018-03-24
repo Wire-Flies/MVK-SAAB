dfb83a3311a89fb3093d50d70295c6f5f19ac37d
jvikstrom <johan.vikstrom7@gmail.com>
2018-03-24 15:51:00 +0100

    Created input handler

---
+++ backend/server/inputHandler.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const classifier = require('./classifier');
+
+/**
+ * Class for handling input
+ */
+class InputHandler {
+    /**
+     * @param {Object} firebaseConnector    Object that can push data to firebase
+     * @param {Number} cutoff               Cutoff for determining when an aircraft is an anomaly
+     */
+    constructor(firebaseConnector, cutoff) {
+        this.firebase = firebaseConnector;
+        this.cutoff = cutoff;
+    }
+
+    /**
+     * Add an additional input source
+     * @param {Object} source Object emitting an aircraft event
+     */
+    addInputSource(source) {
+        source.on('aircraft', (flightId, aircraft) => handleAircraft(flightId, this.cutoff, aircraft, this.firebase));
+    }
+}
+
+/**
+ * @param {String} flightId Id of flight
+ * @param {Number} cutoff   Cutoff when it is an anomaly
+ * @param {Object} aircraft Aircraft to be classified
+ * @param {Object} firebase Object that can push data to firebase
+ */
+function handleAircraft(flightId, cutoff, aircraft, firebase) {
+    classifier.classify(aircraft).then((value) => {
+        if (value > cutoff) {
+            return firebase.saveData(flightId, aircraft);
+        }
+
+        return new Promise((full) => full());
+    }).then(() => {}).catch((err) => console.log(err));
+}
+
+module.exports = {
+    InputHandler: InputHandler,
+};
