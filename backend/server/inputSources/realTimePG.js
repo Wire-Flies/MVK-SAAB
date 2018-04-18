73bcc5f47bcab6043c54e2f378a8160b81a4c50c
jvikstrom <johan.vikstrom7@gmail.com>
2018-04-18 10:40:49 +0200

    Fixed error when iterating over pg result

--- backend/server/inputSources/realTimePG.js
+++ backend/server/inputSources/realTimePG.js
@@ -37,9 +37,11 @@ class RealTimePG extends EventEmitter {
     nextAircraftBatch() {
         const endTime = this.time;
         const startTime = this.time - this.batchTime;
+        console.log('SQL: ' + 'SELECT * FROM flights JOIN flight_data ON flights.flight_id = flight_data.flight_id INNER JOIN airports a ON a.iata_code = flights.schd_from INNER JOIN airports b ON b.iata_code = flights.schd_to WHERE snapshot_id < '+endTime+' AND snapshot_id >= '+startTime+';')
         this.pool.query('SELECT * FROM flights JOIN flight_data ON flights.flight_id = flight_data.flight_id INNER JOIN airports a ON a.iata_code = flights.schd_from INNER JOIN airports b ON b.iata_code = flights.schd_to WHERE snapshot_id < $1 AND snapshot_id >= $2;', [endTime, startTime]).then((flights) => {
             let flightObj = {};
-            _.forEach(flights, (flight) => {
+            console.log('Found: ' + flights.rowCount + ' flights');
+            _.forEach(flights.rows, (flight) => {
                 if (!flightObj[flight.flight_id]) {
                     flightObj[flight.flight_id] = {
                         flight_id: flight.flight_id,
@@ -73,6 +75,7 @@ class RealTimePG extends EventEmitter {
             });
 
             _.forEach(flightObj, (flight) => {
+                console.log('Emitting aircraft: ', flight);
                 this.emit('aircraft', flight);
             });
         }).catch((err) => console.log(err)); // Errors are ignored
