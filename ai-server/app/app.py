cd9bc7b092957d7ff80e5e046e4e807a820f2cdd
Joel Söderberg <joel@n177-p171.eduroam.kth.se>
2018-04-18 11:08:39 +0200

    Added model evaluation to server

--- ai-server/app/app.py
+++ ai-server/app/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
+import numpy as np
+from keras.models import load_model
 import json
 
 app = Flask(__name__)
+
+autoencoder = load_model('./models/model6.h5')
 
 #from app import routes
 @app.route('/')
@@ -13,10 +17,15 @@ def classify():
     print("Request json")
     print(request.json)
     data = request.json
-
+    position = data["position"][0]
+    flight_data = np.array([position["latitude"],position["longitude"],position["altitude"],position["heading"],position["speed"],data["lat_from"],data["long_from"],data["lat_to"],data["long_to"]])
+    value = mean_squared_error(flight_data, autoencoder.predict(flight_data))
     result = {}
-    result["value"] = 0.4 #Return the number of the classification
-    return json.dumps(result) 
+    result["value"] = value #Return the number of the classification
+    return json.dumps(result)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+
+
+
